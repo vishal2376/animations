ff1a7ee17db3b33b5bee182d0665bdb54a2ec5d8
vishal2376 <vishalsingh2376@gmail.com>
2024-05-12 22:08:02 +0530

    init

---
+++ app/src/main/java/com/vishal2376/animations/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.vishal2376.animations
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.vishal2376.animations.ui.theme.AnimationsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AnimationsTheme {
+            }
+        }
+    }
+}
